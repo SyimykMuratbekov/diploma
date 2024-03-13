f0ce56af28ab7b2b304ca427aa352e4b0d478174
SyimykMuratbekov <up9966952@gmail.com>
2024-03-13 13:27:28 +0600

    add new files new structures

---
+++ js/fetchMovies.js
@@ -0,0 +1,154 @@
+class GetMovies {
+	API_URL_MOVIE_DETAILS = "https://kinopoiskapiunofficial.tech/api/v2.2/films/"
+	API_KEY = "8c8e1a50-6322-4135-8875-5d40a5420d86";
+	apiUri = 'https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_100_POPULAR_FILMS'
+	container
+	modalContainer
+
+	constructor(container, modalContainer) {
+		this.container = container
+		this.modalContainer = modalContainer
+	}
+
+	async fetchMovies() {
+		try {
+			const films = await fetch(this.apiUri, {
+				headers: {
+					"Content-Type": "application/json",
+					"X-API-KEY": this.API_KEY,
+				},
+			})
+			if (films.ok) {
+				const body = await films.json()
+				console.log(body)
+				// 
+				this.toHtml(body.films)
+			}
+		} catch (error) {
+			console.log(error)
+		}
+	}
+
+	toHtml(films = []) {
+		films.forEach((film, i) => {
+			const movieEl = document.createElement("div");
+			movieEl.classList.add('movieEl')
+			movieEl.innerHTML = this.html(film)
+			movieEl.addEventListener('click', () => this.modal(film.filmId))
+			this.container.appendChild(movieEl)
+		})
+	}
+
+	/**
+	 * Шаблон для фильомв
+	 */
+	html(movie) {
+		const html = `
+          <div class="movie-poster">
+            <img
+              src="${movie.posterUrl || movie.posterUrlPreview}"
+              alt="${movie.nameRu || movie.nameOriginal || movie.nameEn}" />
+          </div>
+          <div class="movie-content">
+
+            <div class="movie-info">
+
+              <h2 class="movie-title">${movie.nameRu || movie.nameOriginal || movie.nameEn}</h2>
+
+              <div class="movie-items">
+
+                <div class="item">
+                  <span>Год производства: ${movie.year}</span>
+                </div>
+                <div class="item">
+                  <span>Страна: ${movie.countries[0]}</span>
+                </div>
+                <div class="item">
+                  <span>Жанр:  ${movie.genres[0]}</span>
+                </div>
+                <div class="item">
+                  <span>Продолжительность:
+                    <small>97 <sup>мин</sup>. / 02:00</small></span>
+                </div>
+                <div class="item">
+                  <span>Премьера <b>(Кыргызстан)</b>:
+                    <small>2023-10-19</small></span>
+                </div>
+              </div>
+            </div>
+            <!-- Кнопки карточки -->
+            <div class="movie-actions">
+              <a title="Смотреть трейлер" aria-label="Трейлер" target="_blank"
+                href="https://youtu.be/K3HJ0CMBTOk?si=5oEnc6p03UfUVNbL" class="movie-btn">Трейлер
+              </a>
+            </div>
+          </div>
+       `
+		return html
+	}
+
+	/**
+	 * Показывает модальное окно с одним фильмо по его ID
+	 */
+	async modal(id) {
+		const resp = await fetch(this.API_URL_MOVIE_DETAILS + id, {
+			headers: {
+				"Content-Type": "application/json",
+				"X-API-KEY": this.API_KEY,
+			},
+		});
+		const respData = await resp.json();
+
+		this.modalContainer.classList.add("modal--show");
+		document.body.classList.add("stop-scrolling");
+
+		this.modalContainer.innerHTML = `
+			<div class="modal__card">
+				<img class="modal__movie-backdrop" src="${respData.posterUrl}" alt="">
+				<h2>
+					<span class="modal__movie-title">${respData.nameRu}</span>
+					<span class="modal__movie-release-year"> - ${respData.year}</span>
+				</h2>
+				<ul class="modal__movie-info">
+					<div class="loader"></div>
+					<li class="modal__movie-genre">Жанр - ${respData.genres.map((el) => `<span>${el.genre}</span>`)}</li>
+					${respData.filmLength ? `<li class="modal__movie-runtime">Время - ${respData.filmLength} минут</li>` : ''}
+					<li >Сайт: <a class="modal__movie-site" href="${respData.webUrl}">${respData.webUrl}</a></li>
+					<li class="modal__movie-overview">Описание - ${respData.description}</li>
+				</ul>
+				<button type="button" class="modal__button-close">Закрыть</button>
+			</div>
+		`
+		const btnClose = document.querySelector(".modal__button-close");
+		btnClose.addEventListener("click", () => this.closeModal());
+		this.clickOutsideCloseModal()
+	}
+
+	/**
+	 * Закрывает модальное окно при нажатии на кнопку закрыть
+	 */
+	closeModal() {
+		this.modalContainer.classList.remove("modal--show");
+		document.body.classList.remove("stop-scrolling");
+	}
+
+
+	/**
+	 * Закрывает модальное окно при клике по любой области страницы
+	 */
+	clickOutsideCloseModal() {
+
+		window.addEventListener("click", (e) => {
+			if (e.target === this.modalContainer) {
+				this.closeModal();
+			}
+		})
+
+		window.addEventListener("keydown", (e) => {
+			if (e.keyCode === 27) {
+				this.closeModal();
+			}
+		})
+	}
+}
+
---
+++ js/script.js
@@ -0,0 +1,30 @@
+// Получаем все слайды и индекс текущего слайда
+var slides = document.querySelectorAll('.slider-main > .slide');
+var currentSlideIndex = 0;
+
+// Функция для отображения следующего слайда
+function showNextSlide() {
+  // Скрыть текущий слайд
+  slides[currentSlideIndex].style.display = 'none';
+  // Увеличить индекс слайда, или вернуться к началу, если достигнут конец
+  currentSlideIndex = (currentSlideIndex + 1) % slides.length;
+  // Показать следующий слайд
+  slides[currentSlideIndex].style.display = 'block';
+}
+
+// Показать первый слайд
+showNextSlide();
+
+// Установить интервал для автоматического переключения слайдов
+setInterval(showNextSlide, 2500); // 3000 миллисекунд = 3 секунды
+
+const films = new GetMovies(document.getElementById('container'), document.querySelector(".modal"))
+const getfilms = films.fetchMovies()
+
+
+// document.addEventListener('DOMContentLoaded', () => {
+//   const movieId = document.getElementById('movie-id')
+//   if (movieId) {
+//     movieId.addEventListener('click', () => console.log('wda'))
+//   }
+// })
